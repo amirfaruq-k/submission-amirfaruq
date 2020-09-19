d9385d887f013f484c44edec9fb6fbd0bb043bb0
AguzzTN54 <gusagusnanang@gmail.com>
2020-09-19 16:58:03 +0700

    Memperbaiki penggunaan file-loader

--- src/app.js
+++ src/app.js
@@ -2,7 +2,5 @@ import "regenerator-runtime";
 import "./styles/style.css";
 import "./script/component/app-bar.js";
 import main from "./script/view/main.js";
-import webpackLogo from './assets/images/logo.png';
 
-document.querySelector('#imageFigure').src = webpackLogo;
 document.addEventListener("DOMContentLoaded", main);
